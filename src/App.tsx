e6ef086f4e40b5b3472c092509ef6b3c28446ea3
Djordje Cvjetan <djordjecvjetan@gmail.com>
2024-10-01 22:48:06 +0200

    Update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,9 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-// Definicija interfejsa WeatherData
-interface WeatherData {
-  location: {
-    name: string;
-  };
-  current: {
-    temp_c: number;
-    condition: {
-      text: string;
-    };
-  };
-}
-
 function App() {
   const [count, setCount] = useState(0)
-  const [weather, setWeather] = useState<WeatherData | null>(null)
-  const [city, setCity] = useState('Belgrade')
-
-  const API_KEY = 'YOUR_API_KEY'
-
-  useEffect(() => {
-    const fetchWeather = async () => {
-      try {
-        const response = await axios.get<WeatherData>(`https://api.weatherapi.com/v1/current.json`, {
-          params: {
-            key: API_KEY,
-            q: city,
-            aqi: 'no'
-          }
-        });
-        setWeather(response.data)
-      } catch (error) {
-        console.error('Greška prilikom preuzimanja vremenske prognoze:', error);
-      }
-    }
-
-    fetchWeather()
-  }, [city])
 
   return (
     <>
@@ -57,18 +20,6 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </div>
-
-      <div className="weather">
-        <h2>Vremenska prognoza za: {city}</h2>
-        {weather ? (
-          <>
-            <p>Temperatura: {weather.current.temp_c}°C</p>
-            <p>Opis: {weather.current.condition.text}</p>
-          </>
-        ) : (
-          <p>Učitavanje vremenske prognoze...</p>
-        )}
       </div>
 
       <p className="read-the-docs">
