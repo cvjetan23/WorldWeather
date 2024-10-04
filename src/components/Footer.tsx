d8431ec7f992df6637c27d68ea02770f6b5cd655
Djordje Cvjetan <djordjecvjetan@gmail.com>
2024-10-04 10:14:19 +0200

    Make Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2024 WeatherApp. All Rights Reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
