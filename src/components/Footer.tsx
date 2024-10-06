d27c06378a8c091bcc94d89d0ef7fa9fe66fba96
Djordje Cvjetan <djordjecvjetan@gmail.com>
2024-10-06 23:58:04 +0200

    Add style to Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer>
-      <p>&copy; 2024 WeatherApp. All Rights Reserved.</p>
+    <footer className="bg-gray-800 text-white py-4 text-center">
+      <p className="text-sm">&copy; 2024 WeatherApp. All Rights Reserved.</p>
     </footer>
   );
 }
